c21e61de0180196e4932dfc229584ffea2002d41
neet-no1 <no1.neet.professional@gmail.com>
2021-03-18 13:03:01 +0900

    initial commit

---
+++ src/main/java/jp/co/suyama/menu/deliver/MenuDeliverApiApplication.java
@@ -0,0 +1,13 @@
+package jp.co.suyama.menu.deliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuDeliverApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuDeliverApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/co/suyama/sandwich/SandwichApiApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.suyama.sandwich;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandwichApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
